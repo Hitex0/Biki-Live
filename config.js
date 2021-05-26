7af733acd15bb45ed6305937807207b33a7bafe3
〢KAKASHI〢 <72246057+kakashidracq@users.noreply.github.com>
2021-05-26 22:48:53 +0545

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     token: "process.env.BIKI_GG",
     channel: "process.env.BIKI_YOUTUBE",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video! Go check it out **{title}**!\n{url}",
-    channel_id: "UCj4UnidqX7S8aWUcTUV0mKA",
+    channel_id: "process.env.BIKI_YT",
     watchInterval: 3000
 }
