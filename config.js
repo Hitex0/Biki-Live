eeea20d70cedd3a0b72a688b3a16484974bf8dea
〢KAKASHI〢 <72246057+kakashidracq@users.noreply.github.com>
2021-05-26 13:37:14 +0545

    de

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    CHANNEL_WELCOME: "840767573519237130",
+    CHANNEL_WELCOME: "process.env.BIKI_WELCOME",
     token: "process.env.BIKI_GG",
-    channel: "805090988925517837",
+    channel: "process.env.BIKI_YOUTUBE",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video! Go check it out **{title}**!\n{url}",
     channel_id: "UCj4UnidqX7S8aWUcTUV0mKA",
     watchInterval: 3000
