8d2db3985d43cb44f8ca516bbaf117fb4aaff410
Vitex Official <84669852+Hitex0@users.noreply.github.com>
2021-06-01 14:43:39 +0545

    123

--- src/Commands/Fun/snake.js
+++ src/Commands/Fun/snake.js
@@ -1,13 +1,29 @@
 const { MessageEmbed } = require('discord.js');
 const SnakeGame = require('snakecord')
+const Command = require('../../Structures/Command');
 
-const snakeGame = new SnakeGame({
-    title: 'Snake Game',
-    color: "BLUE",
-    timestamp: true,
-    gameOverTitle: ":DG_amongus:Game Over"
-});
-const uchannel = "848514124727058485"
-const aschannel = this.client.channels.cache.get(uchannel);
-aschannel.send(`Used Snake Game`)
-return snakeGame.newGame(message)
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['snake'],
+			description: 'snake game',
+			category: '<:amongus:848974047308283914>__General__'
+		});
+	}
+
+	async run(message) {
+		const snakeGame = new SnakeGame({
+            title: 'Snake Game',
+            color: "BLUE",
+            timestamp: true,
+            gameOverTitle: ":DG_amongus:Game Over"
+        });
+        const uchannel = "848514124727058485"
+        const aschannel = this.client.channels.cache.get(uchannel);
+        aschannel.send(`Used Snake Game`)
+        return snakeGame.newGame(message)
+        
+	}
+
+};
---
+++ src/Commands/General/say.js
@@ -0,0 +1,29 @@
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['say'],
+			description: 'send bot msg',
+			category: '<:amongus:848974047308283914>__General__'
+		});
+	}
+
+	async run(message) {
+		if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('You dont have permission for this command.');
+        if (!args[0]) return message.reply("Please specify message that bot should say.")
+        let msg;
+        let textChannel = message.mentions.channels.first()
+        message.delete()
+
+        if(textChannel) {
+            msg = args.slice(1).join(" ");
+            textChannel.send(msg)
+        } else {
+            msg = args.join(" ");
+            message.channel.send(msg)
+            }
+	}
+
+};
