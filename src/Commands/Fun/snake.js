c6ff6690e380553adbcd53fc5f55a9058238ec38
Vitex Official <84669852+Hitex0@users.noreply.github.com>
2021-06-01 14:57:03 +0545

    Update snake.js

--- src/Commands/Fun/snake.js
+++ src/Commands/Fun/snake.js
@@ -19,9 +19,7 @@ module.exports = class extends Command {
             timestamp: true,
             gameOverTitle: ":DG_amongus:Game Over"
         });
-        const uchannel = "848514124727058485"
-        const aschannel = this.client.channels.cache.get(uchannel);
-        aschannel.send(`Used Snake Game`)
+        
         return snakeGame.newGame(message)
         
 	}
