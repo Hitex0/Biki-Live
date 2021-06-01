5f5fdf7c691a2e5d7a6e6b4fed9042798409bfa4
Vitex Official <84669852+Hitex0@users.noreply.github.com>
2021-06-01 12:35:26 +0545

    Update mailbox.js

--- src/Commands/General/mailbox.js
+++ src/Commands/General/mailbox.js
@@ -7,26 +7,28 @@ module.exports = class extends Command {
 		super(...args, {
 			aliases: ['send'],
 			description: 'This provides send msg on privite',
-			category: '<:amongus:848974047308283914>__General__'
+			category: '<:amongus:848974047308283914>__General__',
+            userPerms: ['KICK_MEMBERS'],
+            botPerms: ['ADMINISTRATOR'],
+            guildOnly: true
 		});
 	}
 
 	async run(message) {
         const mention = message.mentions.users.first();
-        if (!message.member.hasPermission("ADMINISTRATOR")) return;
         if (mention == null) { return; }
         message.delete();
         const sendingMessage = message.content.slice (28);
         const SendingEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(`You Have a New Message From **${message.guild.name} Server**!`)
-        .setURL('')
-        .setAuthor(``, '', '')
-        .setDescription(`${sendingMessage}`)
+        .setColor('RED')
+        .setTitle(`**Vitex Community Server Alat🔔**!`)
+        //.setURL('')
+        //.setAuthor(``, '', '')
+        //.setDescription(`${sendingMessage}`)
         .setThumbnail('https://img.pngio.com/comments-inbox-message-one-message-icon-png-message-512_512.png')
-        .setImage('')
-        .setTimestamp()
-        .setFooter('Generated From vitex Bot!', 'https://cdn.discordapp.com/attachments/844295450361724929/847765572812275722/Vitex.png');
+        //.setImage('')
+        //.setTimestamp()
+       // .setFooter('Generated From vitex Bot!', 'https://cdn.discordapp.com/attachments/844295450361724929/847765572812275722/Vitex.png');
         mention.send (SendingEmbed);
         message.channel.send ("Done✅");
         console.log(`Message : ${sendingMessage} Genereted by ${message.author.tag} to ${mention} from ${message.guild.name} ✅ \n`);
