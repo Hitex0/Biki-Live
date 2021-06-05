eab82ace2e08a63db49a5bc10aff2a25bfe4e863
Vitex Official <84669852+Hitex0@users.noreply.github.com>
2021-06-05 09:52:12 +0545

    Update respect.js

--- src/Commands/General/respect.js
+++ src/Commands/General/respect.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
 	}
 
 	async run(message) {
-        message.channel.send(`@${message.guild.name} Paid Respect😇`)
+        message.channel.send(`You Paid Respect😇`)
 		
 	}
 
